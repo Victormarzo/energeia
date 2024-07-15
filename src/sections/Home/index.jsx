66b8cda3b9b695e95a111f10fc2f68deca90becd
victormarzo <victor.marzo.monteiro@gmail.com>
2024-07-15 16:37:58 -0300

    test

--- src/sections/Home/index.jsx
+++ src/sections/Home/index.jsx
@@ -3,7 +3,7 @@ import "./home.css"
 
 const Home = ()=>{
     return (
-        <div className="home" >Essa é a home</div>
+        <div className="home" >Essa é a home my man</div>
     )
 }
 export default Home
